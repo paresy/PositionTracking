ca915257d0248727a9f36b441af440fc644627dd
Michael Maroszek <maroszek@gmx.net>
2021-05-10 12:42:14 +0200

    add missing type hint

--- PositionTracking/module.php
+++ PositionTracking/module.php
@@ -71,7 +71,7 @@ class PositionTracking extends WebHookModule
         }
     }
 
-    public function SendLocation($location)
+    public function SendLocation(string $location)
     {
         $hcID = IPS_GetInstanceListByModuleID('{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}')[0];
         WC_PushMessage($hcID, '/hook/position_tracking/' . $this->InstanceID, $location);
