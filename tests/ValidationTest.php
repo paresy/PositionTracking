ea3458b3d643d4543654ce6cb3e599d6e0b1dd95
Michael Maroszek <maroszek@gmx.net>
2021-08-27 15:34:15 +0200

    fix validation test name

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 include_once __DIR__ . '/stubs/Validator.php';
 class ValidationTest extends TestCaseSymconValidation
 {
-    public function testValidateVictron(): void
+    public function testValidatePositionTracking(): void
     {
         $this->validateLibrary(__DIR__ . '/..');
     }
-    public function testValidateVEDirectModule(): void
+    public function testValidatePositionTrackingModule(): void
     {
         $this->validateModule(__DIR__ . '/../PositionTracking');
     }
